71e9aa189803e64ed2ae851b6a6dead97c518307
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-06-01 17:11:01 +0200

    improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -15,26 +15,37 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
       <h2 className="text-6xl font-bold text-center my-16">Try swapping with our new hook</h2>
-      <div className="bg-gray-900 p-6 w-xl rounded-xl shadow-md flex flex-col items-center space-y-4">
-        <div className="flex flex-col w-full bg-gray-700 rounded-xl px-14 py-6">
+      <div className="bg-gray-900 p-4 w-[600px] rounded-2xl shadow-md flex flex-col items-center">
+        <div className="flex flex-col w-full bg-gray-700 rounded-2xl p-4 -mb-5">
           Sell
-          <div className="flex flex-row space-x-14 py-8">
-            <input className="bg-transparent focus:bg-transparent focus:outline-none" type="text" placeholder="Amount" />
+          <div className="flex flex-row w-full space-x-14 py-8">
+            <input
+              className="flex-auto bg-transparent focus:bg-transparent focus:outline-none text-2xl"
+              type="text"
+              placeholder="Amount"
+            />
             <div className="flex bg-pink-600 px-5 py-3 rounded-full items-center justify-center">{crypto1}</div>
           </div>
         </div>
         <div className="flex-1 bg-gray-700 rounded-xl">
-          <button className="btn btn-neutral" onClick={swap}>
-            change
+          <button className="relative z-50 btn btn-neutral" onClick={swap}>
+            <Image width={20} height={20} src="https://www.svgrepo.com/show/119597/up-and-down-arrows.svg" alt="Swap" />
           </button>
         </div>
-        <div className="flex flex-col w-full bg-gray-700 rounded-xl px-14 py-6">
+        <div className="flex flex-col w-full bg-gray-700 rounded-2xl p-4 -mt-5">
           Buy
-          <div className="flex flex-row space-x-14 py-8">
-            <input className="bg-transparent focus:bg-transparent focus:outline-none" type="text" placeholder="Amount" />
+          <div className="flex flex-row w-full space-x-14 py-8">
+            <input
+              className="flex-auto bg-transparent focus:bg-transparent focus:outline-none text-2xl"
+              type="text"
+              placeholder="Amount"
+            />
             <div className="flex bg-pink-600 px-5 py-3 rounded-full items-center justify-center">{crypto2}</div>
           </div>
         </div>
+        <button className="flex w-full bg-pink-600 hover:bg-pink-700 rounded-2xl justify-center items-center text-xl p-4 mt-2">
+          Swap
+        </button>
       </div>
     </main>
   );
