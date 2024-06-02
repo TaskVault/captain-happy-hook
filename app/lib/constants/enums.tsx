77057fd7d30fbf9663197a0ac601b74973a10864
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-06-02 03:45:56 +0200

    Added swap states

---
+++ app/lib/constants/enums.tsx
@@ -0,0 +1,9 @@
+export enum SwapState {
+  ApproveSwapRouter = "APPROVE_SWAP_ROUTER",
+  WaitingApproveSwapRouter = "WAITING_APPROVE_SWAP_ROUTER",
+  ApproveHook = "APPROVE_HOOK",
+  WaitingApproveHook = "WAITING_APPROVE_HOOK",
+  Swap = "SWAP",
+  WaitingSwap = "WAITING_SWAP",
+  CompletedSwap = "COMPLETED_SWAP",
+}
